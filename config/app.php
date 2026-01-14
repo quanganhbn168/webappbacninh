9d3d918a2e11763f2a31102293f23032e9e0e486
Quang <quanganhbn168@gmail.com>
2026-01-14 09:15:07 +0700

    fix: app config import and clean up missing providers

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\ServiceProvider;
+
 return [
 
     /*
@@ -132,10 +134,10 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
+        // App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        // App\Providers\EventServiceProvider::class,
+        // App\Providers\RouteServiceProvider::class,
         App\Providers\SettingsServiceProvider::class,
     ])->toArray(),
 ];
